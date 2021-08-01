c86ed5d229988e556d1a5f821c3a1701216ba61f
xiaoyou66 <xiaoyou2333@foxmail.com>
2021-08-01 17:49:22 +0800

    init: 初始化

---
+++ error/404.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="author" content="journey.ad">
+    <meta name="copyright" content="journey.ad">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="Robots" content="NOINDEX, NOFOLLOW, NOARCHIVE">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="theme-color" content="#f33">
+    <title>404 Not Found</title>
+    <style>
+        a,abbr,acronym,address,applet,article,aside,audio,b,big,blockquote,body,canvas,caption,center,cite,code,dd,del,details,dfn,div,dl,dt,em,embed,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,header,hgroup,html,i,iframe,img,ins,kbd,label,legend,li,mark,menu,nav,object,ol,output,p,pre,q,ruby,s,samp,section,small,span,strike,strong,sub,summary,sup,table,time,tt,u,ul,var,video{margin:0;padding:0;border:0;vertical-align:baseline;font:inherit;font-size:100%}
+        tbody,td,tfoot,th,thead,tr{margin:0;padding:0;border:0;font:inherit;font-size:100%}
+        article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}
+        body{line-height:1}
+        ol,ul{list-style:none}
+        q:after,q:before{content:'';content:none}
+        body{width:100%}
+        body:after,body:before{display:table;content:""}
+        body:after{clear:both}
+        body,html{height:100%}
+        html{height:100%;max-height:100%}
+        body{color:#333;font-size:1em;font-family:ff-tisa-web-pro-1,ff-tisa-web-pro-2,"Lucida Grande","Hiragino Sans GB","Hiragino Sans GB W3","Microsoft YaHei","WenQuanYi Micro Hei",sans-serif;-webkit-font-smoothing:antialiased}
+        ::selection{background:rgba(255,255,255,.3)}
+        ::-moz-selection{background:rgba(255,255,255,.3)}
+        a{color:#df9c81;text-decoration:none}
+        a:hover{color:#d06c44;-webkit-transition:.5s;-moz-transition:.5s;-o-transition:.5s;-ms-transition:.5s}
+        h1,h2,h3,h4,h5{margin-top:1em;margin-bottom:.5em;color:#333;font-weight:lighter;font-family:ff-tisa-web-pro-1,ff-tisa-web-pro-2,"Lucida Grande","Hiragino Sans GB","Hiragino Sans GB W3","Microsoft YaHei","WenQuanYi Micro Hei",sans-serif;-webkit-font-smoothing:antialiased}
+        h1{margin-top:0;letter-spacing:.05em;font-size:1.8em;line-height:1.2em}
+        h2{font-size:1.6em}
+        h3{font-size:1.4em}
+        h4{font-size:1.2em}
+        h4{font-size:1.1em}
+        h5{font-size:1em}
+        p{margin-bottom:1.3em;line-height:1.7em}
+        em{font-style:italic}
+        ol,ul{margin:0 0 .3em 1em}
+        ol li,ul li{margin:0 0 .2em 0;line-height:1.6em}
+        ol ol,ol ul,ul ol,ul ul{margin:.1em 0 .2em 2em}
+        ol{list-style-type:decimal}
+        ul{list-style-type:disc}
+        .author a,.date a,.tags a,.time a{color:#666}
+        .author a:hover,.date a:hover,.tags a:hover,.time a:hover{color:#d06c44}
+        .footer{position:absolute;bottom:0;left:0;width:100%;color:#b3b3b3;font-size:.7em}
+        .logo{width:25%;border:3px solid #fff;border-radius:50%;box-shadow:0 0 1px 1px rgba(0,0,0,.3);transition:all .5s}
+        .logo:hover{border:3px solid #5ba4e5;transition:all .5s;-webkit-transform:rotate(360deg);-moz-transform:rotate(360deg);-o-transform:rotate(350deg);transform:rotate(360deg);-ms-transform:rotate(360deg)}
+        hr{border:none}
+        .panel-title{margin:0 0 15px 0;color:#fff;letter-spacing:4px;font-size:2.5em}
+        .panel-cover{position:fixed;z-index:900;display:block;width:100%;height:100%;max-width:none;background-size:cover}
+        .panel-cover--overlay{position:absolute;top:0;right:0;bottom:0;left:0;z-index:0;display:block;background-color:rgba(68,68,68,.6);background-image:-webkit-linear-gradient(-410deg,rgba(68,68,68,.6) 20%,rgba(0,0,0,.9));background-image:linear-gradient(140deg,rgba(68,68,68,.6) 20%,rgba(0,0,0,.9))}
+        .panel-cover__logo{margin:0 0 0 20px;margin-bottom:.2em}
+        .panel-cover__description{margin:0 30px;font-weight:400}
+        .panel-cover__divider{margin:20px auto;width:50%;border-top:1px solid rgba(255,255,255,.14)}
+        .panel-cover__divider--secondary{width:15%}
+        .panel-main{display:table;width:100%;height:100%}
+        .panel-main__inner{position:relative;z-index:800;display:table-cell;padding:0 60px;vertical-align:middle}
+        .panel-main__content{margin:0 auto;max-width:620px}
+        .panel-inverted{color:#fff;text-align:center;text-shadow:0 1px 1px rgba(0,0,0,.4);font-weight:100}
+        .panel-inverted a{color:#fff}
+        .cover-slate{background-color:rgba(60,91,147,.6);background-image:-webkit-linear-gradient(-410deg,rgba(61,66,96,.6) 20%,rgba(21,23,34,.8));background-image:linear-gradient(140deg,rgba(61,66,96,.6) 20%,rgba(21,23,34,.8))}
+        nav{position:relative;display:inline-block}
+        .navigation__item a{position:relative;display:block;border-color:#fff;color:#fff;opacity:.8}
+        .navigation__item a:hover{border-color:rgba(255,255,255,.3);background:rgba(255,255,255,.3);color:#fff;opacity:1;transition:all .3s}
+        .navigation--social a{padding:6px 8px 6px 9px;border:0}
+        .navigation--social a .label{display:none}
+        .navigation--social a .icon{display:block;font-size:1.7em}
+        i{font-weight:400;font-style:normal;font-size:18px;font-family:entypo}
+        :focus{outline:0}
+        @media all and (max-width:1100px){.panel-title{font-size:2em}
+            .panel-cover__description{margin:0 10px;font-size:.9em}
+        }
+        @media all and (max-width:960px){
+            .panel-main{position:relative;display:table}
+            .panel-main__inner{display:table-cell;padding:60px 10%}
+            .panel-cover__description{display:block;margin:0 auto;max-width:600px}
+            .panel-cover{width:100%;height:100%;background-position:center center}
+            .panel-cover.panel-cover--collapsed .panel-cover__logo{width:60px;border-width:2px}
+            .panel-cover.panel-cover--collapsed .panel-cover__description{display:none}
+            .panel-cover.panel-cover--collapsed .panel-cover__divider{display:none;margin:1em auto}
+            .cover-navigation .navigation li{margin-bottom:.4em;width:80%}
+            .cover-navigation.navigation--social .navigation li{display:inline-block;width:25.8%}
+        }
+    </style>
+</head>
+<body>
+<div id="main" class="panel-cover">
+<!--    <span class="mobile btn-mobile-menu">-->
+<!--      <i class="fa fa-list btn-mobile-menu__icon" />-->
+<!--      <i class="fa fa-angle-up btn-mobile-close__icon hidden" />-->
+<!--    </span>-->
+    <div class="panel-cover" style="background-image: url(<?php echo setting_template()."/static/image/error/bg.jpg" ?>)">
+        <div class="panel-main">
+            <div class="panel-main__inner panel-inverted">
+                <div class="panel-main__content">
+                    <a href="/" title="回到首页"><img src="<?php echo setting_template()."/static/image/error/few.jpg" ?>" width="150" alt="Noisky logo" class="panel-cover__logo logo"></a>
+                    <h1 class="panel-cover__title panel-title">
+                        <a href="/" title="link to homepage for noisky">404</a>
+                    </h1>
+
+                    <hr class="panel-cover__divider">
+                    <p class="panel-cover__description">
+                        对不起，您要找的页面不存在!
+                    </p>
+                    <hr class="panel-cover__divider panel-cover__divider--secondary">
+
+                    <p class="footer">
+                        © 2021  |  <a style="TEXT-DECORATION: none" href="/" rel="nofollow"><span style="color: #b3b3b3" ><?php  setting_index();setting_index_site_name(); ?></span></a>
+                    </p>
+                </div>
+            </div>
+            <div class="panel-cover--overlay cover-slate">
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
